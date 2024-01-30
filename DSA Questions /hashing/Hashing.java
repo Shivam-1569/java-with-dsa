fe2db7e2320055561aeaac1c0a4a46838a26072f
Shivam Pandey <shivampandey01032003@gmail.com>
2024-01-30 18:03:51 +0530

    Update Hashing.java

--- DSA Questions /hashing/Hashing.java
+++ DSA Questions /hashing/Hashing.java
@@ -2,8 +2,15 @@
  * Hashing
  */
 public class Hashing {
-    
     public static void main(String[] args) {
-        
+        int[] arr = {2,3,4,3,4,3,4,3,5,6};
+        HashMap<Integer, Integer> op = new HashMap<>();
+        for(int num: arr){
+            if(op.containsKey(num)){
+                op.put(num, op.get(num)+1);
+            }else{
+                op.put(num, 1);
+            }
+        }
     }
 }
